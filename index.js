15614b8d95c2223891cd0f64ff35332358ae8d7c
Khyati Gupta <khyatig0206@gmail.com>
2024-10-05 21:33:38 +0530

    o

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-// import path from 'path';
-// import { fileURLToPath } from 'url';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(cors({
   origin: '*',
